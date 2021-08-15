54a1b75a41f1313beddee1e98eae0c1c5bb5fc40
Leon Völlinger <leonvoellinger@web.de>
2021-08-15 19:04:04 +0200

    initial commit

---
+++ src/test/java/com/example/praktitwitter/PraktiTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktitwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktiTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
